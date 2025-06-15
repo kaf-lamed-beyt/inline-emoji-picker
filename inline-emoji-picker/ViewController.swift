a1c6d79e2a3047df18e5f5e1e25b42611ea01c47
kaf-lamed-beyt <belac335@gmail.com>
2025-06-15 14:40:16 +0100

    Initial Commit

---
+++ inline-emoji-picker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  inline-emoji-picker
+//
+//  Created by Seven on 15/06/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ inline-emoji-pickerTests/inline_emoji_pickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  inline_emoji_pickerTests.swift
+//  inline-emoji-pickerTests
+//
+//  Created by Seven on 15/06/2025.
+//
+
+import Testing
+@testable import inline_emoji_picker
+
+struct inline_emoji_pickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
